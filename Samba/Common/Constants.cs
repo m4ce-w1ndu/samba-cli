ce0b052fae8f753bfc5fc720b7d3c165052cf070
m4ce-w1ndu <m4ce.w1ndu@icloud.com>
2023-05-31 00:21:19 +0200

    Added constants and utilities

--- Samba/Common/Constants.cs
+++ Samba/Common/Constants.cs
@@ -9,5 +9,20 @@
         /// Default users CSV file delimiter
         /// </summary>
         public const string DEFAULT_DELIMITER = ";";
+
+        /// <summary>
+        /// Default configuration file path
+        /// </summary>
+        public const string DEFAULT_CFG_PATH = "/etc/ad/ad.conf";
+
+        /// <summary>
+        /// Default LDAP port
+        /// </summary>
+        public const int DEFAULT_LDAP_PORT = 389;
+
+        /// <summary>
+        /// Failure exit code based on Unix standard
+        /// </summary>
+        public const int EXIT_FAILURE = 1;
     }
 }
--- Samba/Common/Utilities.cs
+++ Samba/Common/Utilities.cs
@@ -1,10 +1,43 @@
-﻿namespace Samba.Common
+﻿using Serilog;
+using Serilog.Core;
+using System.Text;
+
+namespace Samba.Common
 {
+    /// <summary>
+    /// Contains common utilities
+    /// </summary>
+    public static class CommonUtilities
+    {
+        /// <summary>
+        /// Project logger instance
+        /// </summary>
+        public static Logger Logger { get; set; } = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+    }
+
     public static class StringUtilities
-    { 
+    {
+        public const char DOMAIN_SEPARATOR = '.';
+
         public static string NormalizeName(string name)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Get LDAP search base from domain FQDN
+        /// </summary>
+        /// <param name="domain">Domain FQDN</param>
+        /// <returns>Search base for this domain</returns>
+        public static string GetSearchBaseFromFQDN(string domain)
+        {
+            var sb = new StringBuilder();
+            var components = domain.Split(DOMAIN_SEPARATOR);
+
+            foreach (var (component, i) in components.Select((value, i) => (value, i)))
+                sb.Append(i < components.Length ? $"dc={component}," : $"dc={component}");
+
+            return sb.ToString();
         }
     }
 }
