9a038e2c5560cb68fddf8a2c765af863e9575ecb
m4ce-w1ndu <m4ce.w1ndu@icloud.com>
2023-06-11 13:49:07 +0200

    Changed project structure to a more modular approach. Implemented ICommand interface

---
+++ Samba/Command/ICommand.cs
@@ -0,0 +1,28 @@
+﻿namespace Samba.Command
+{
+    /// <summary>
+    /// Command interface to implement on every type of command
+    /// </summary>
+    public interface ICommand
+    {
+        /// <summary>
+        /// Holds the execution result of the issued command
+        /// </summary>
+        public ExecutionResultState ExecutionResult { get; init; }
+
+        /// <summary>
+        /// Runs the command
+        /// </summary>
+        public void Run();
+    }
+
+    /// <summary>
+    /// Possible command states
+    /// </summary>
+    public enum ExecutionResultState
+    {
+        Ok,
+        Error,
+        Stopped
+    }
+}
